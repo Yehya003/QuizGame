9f01359a5a65c2eccd4fe8b46ece593dad59b3af
Yehya003 <yehya.alkhatib0003@stud.hkr.se>
2019-04-23 09:25:54 +0200

    Add libraries

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,12 +10,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+        /*Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
         root.getStylesheets().add(getClass().getResource("StyleSheet.css").toExternalForm());
         primaryStage.setTitle("Quiz me");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(true);
-        primaryStage.show();
+        primaryStage.show();*/
+        StageManager.getInstance().getLogin();
     }
     public static void main(String[] args) {
         launch(args);

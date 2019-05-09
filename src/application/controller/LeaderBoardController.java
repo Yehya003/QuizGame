c7df7f256317c8468590e222d4692908635dbc16
Ahmed Elkady <ahmedelkady819@gmail.com>
2019-05-10 01:48:24 +0200

    Fixed the alignment of the columns content and added some comments

--- src/application/controller/LeaderBoardController.java
+++ src/application/controller/LeaderBoardController.java
@@ -38,13 +38,27 @@ public class LeaderBoardController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //establishing connection with the database class
         DatabaseConnector connector = new DatabaseConnector();
+        // instructing the columns to read the values from these quiz class specific attributes
+        // that will happen after we set the items inside the tableView by we must instruct first the columns and tell
+        // them where to read from.
         username.setCellValueFactory(new PropertyValueFactory<>("userName"));
         score.setCellValueFactory(new PropertyValueFactory<>("score"));
         category.setCellValueFactory(new PropertyValueFactory<>("category"));
         duration.setCellValueFactory(new PropertyValueFactory<>("duration"));
-       try {
-           tableView.setItems(connector.getTheHighestScores());
-       }catch(Exception e){e.printStackTrace();}
-       }
+       //Setting the column content alignment to be in the center
+        username.setStyle("-fx-alignment: CENTER;");
+        score.setStyle("-fx-alignment: CENTER;");
+        category.setStyle("-fx-alignment: CENTER;");
+        duration.setStyle("-fx-alignment: CENTER;");
+
+        try {
+            //setting the items inside the table by getting the returning value from the database method that
+            // retrieves them from the database
+            tableView.setItems(connector.getTheHighestScores());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

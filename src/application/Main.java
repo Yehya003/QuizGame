6950a28edbeb3d5cf93179cf1e53f8c789ce7c18
Ahmed Elkady <ahmedelkady819@gmail.com>
2019-05-21 23:05:13 +0200

    A lot of work on the interface and ui design, check it out

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,21 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        /*Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
-        root.getStylesheets().add(getClass().getResource("StyleSheet.css").toExternalForm());
-        primaryStage.setTitle("Quiz me");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(true);
-        primaryStage.show();*/
+        //Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        //root.getStylesheets().add(getClass().getResource("StyleSheet.css").toExternalForm());
+        //primaryStage.setTitle("Quiz me");
+        //primaryStage.setScene(new Scene(root));
+        //primaryStage.setResizable(true);
+        //primaryStage.show();
         StageManager.getInstance().getLogin();
     }
     public static void main(String[] args) {

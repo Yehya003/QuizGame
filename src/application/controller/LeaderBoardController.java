80ccaeb0be44e34bbc7bc291644127917aa4f5cf
Ahmed Elkady <ahmedelkady819@gmail.com>
2019-05-21 23:04:25 +0200

    Pie chart & Line chart & smart Filter& new side pane buttons.A lot of ny functions such as the side pane with all the buttons and smart filtering also the statistics and pie chart

--- src/application/controller/LeaderBoardController.java
+++ src/application/controller/LeaderBoardController.java
@@ -4,14 +4,27 @@ import application.DatabaseConnector;
 import application.StageManager;
 import application.model.Quiz;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.animation.Animation;
+import javafx.animation.Transition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.layout.AnchorPane;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,6 +32,10 @@ public class LeaderBoardController implements Initializable {
 
     @FXML
     private TableView<Quiz> tableView;
+
+    @FXML
+    private JFXCheckBox top10;
+
     @FXML
     private TableColumn<Quiz, String> username;
 
@@ -30,24 +47,35 @@ public class LeaderBoardController implements Initializable {
 
     @FXML
     private TableColumn<Quiz, Integer> duration;
-
     @FXML
-    private JFXButton back;
+    private AnchorPane filterPane;
     @FXML
-    private LineChart<?, ?> lineChart;
-    XYChart.Series<?,?> myChart = new XYChart.Series<>();
+    private AnchorPane mainPane;
+    @FXML
+    private JFXComboBox<String> from;
+    @FXML
+    private JFXComboBox<String> to;
+    @FXML
+    private LineChart<String, Integer> lineChart;
+    @FXML
+    private PieChart pieChart;
+    boolean populated = false;
+    XYChart.Series<String, Integer> myChart = new XYChart.Series<>();
+    DatabaseConnector connector = new DatabaseConnector();
 
-    public void backBtnPressed() {
+    public void mainMenuPressed() {
         StageManager.getInstance().getMainMenu();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        DatabaseConnector connector = new DatabaseConnector();
+
+        //setting the column input that corresponds to the variables of the quiz object
         username.setCellValueFactory(new PropertyValueFactory<>("userName"));
         score.setCellValueFactory(new PropertyValueFactory<>("score"));
         category.setCellValueFactory(new PropertyValueFactory<>("category"));
         duration.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        //Setting the alignment to the middle
         username.setStyle("-fx-alignment: CENTER;");
         score.setStyle("-fx-alignment: CENTER;");
         category.setStyle("-fx-alignment: CENTER;");
@@ -56,8 +84,64 @@ public class LeaderBoardController implements Initializable {
         try {
             tableView.setItems(connector.getTheHighestScores());
             tableView.setEditable(false);
+            //getting the return value of the method and saving it to the XYChart
+            myChart = connector.getDataForStatistics();
+            //populating the pie chart from the database connector after it returns observable list full of data
+            pieChart.setData(connector.getPlayedCategoriesRatio());
+            //plugging in the data from the XYChart to view it in the line chart
+            lineChart.getData().add(myChart);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public void filterPressed() {
+        GaussianBlur blur = new GaussianBlur();
+        mainPane.setEffect(blur);
+        // mainPane.setOpacity(0.71);
+        populateComboBox();
+        filterPane.setLayoutY(190);
+        filterPane.setVisible(true);
+        mainPane.setDisable(true);
+    }
+
+    public void populateComboBox() {
+        if (populated == false) {
+            for (int i = 0; i < 11; i++) {
+                from.getItems().add(String.valueOf(i));
+                to.getItems().add(String.valueOf(i));
+                populated = true;
+            }
+        }
+    }
+
+    public void okBtnPressed() {
+        try {
+            if (top10.isPressed()) {
+                tableView.setItems(connector.smartFilterData(from.getSelectionModel().getSelectedItem(), to.getSelectionModel().getSelectedItem(), true));
+
+            } else {
+                tableView.setItems(connector.smartFilterData(from.getSelectionModel().getSelectedItem(), to.getSelectionModel().getSelectedItem(), false));
+            }
+            filterPane.setVisible(false);
+            mainPane.setEffect(null);
+            mainPane.setDisable(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cancellBtnPressed() {
+        filterPane.setVisible(false);
+        mainPane.setDisable(false);
+        mainPane.setEffect(null);
+    }
+    public void editBtnPressed() {
+        StageManager.getInstance().getEditPlayer();
+    }
+    public void logOutPressed() {
+        StageManager.getInstance().getLogin();
+    }
+
+
 }
